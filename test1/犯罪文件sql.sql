548a9f817b30554560f5cfbd2583f11ae4588404
star123moon <43492756+star123moon@users.noreply.github.com>
2023-08-31 15:06:37 +0800

    Update 犯罪文件sql.sql

--- test1/犯罪文件sql.sql
+++ test1/犯罪文件sql.sql
@@ -1,6 +1,18 @@
+--建表
+CREATE TABLE `test1_crime` (
+  `incident_id` int NOT NULL AUTO_INCREMENT COMMENT '事件编码',
+  `offence_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '犯罪代号',
+  `dispatch_time` datetime DEFAULT NULL COMMENT '派单时间',
+  `victims` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '受害人数',
+  `crime_name1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '罪名1',
+  `crime_name2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '罪名2',
+  `crime_name3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '罪名3',
+  `city` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '城市',
+  `start_date_time` datetime DEFAULT NULL COMMENT '发生时间',
+  PRIMARY KEY (`incident_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=201400057 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-------------1
+------------1   题目一部分在CrimeController里面采取代码先删除一部分数据
 
 DELETE  from test1_crime where (incident_id, offence_code,start_date_time) in (
 select a.incident_id, a.offence_code,a.start_date_time from (
